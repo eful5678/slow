ec6461cf5d271195aa558031f098016594da463b
Jin <khjkhj1212@hanmail.net>
2021-06-27 22:51:53 +0900

    lambda

---
+++ Java/Baek_Java8/java8to11/src/main/java/com/example/java8to11/Foo.java
@@ -0,0 +1,21 @@
+package com.example.java8to11;
+
+public class Foo {
+
+    private static void main(String[] args) {
+        // 익명 내부 클래스 anonymous inner Class
+        // RunSomething runSomething = new RunSomething(){
+
+        // @Override
+        // public void doIt() {
+        // // TODO Auto-generated method stub
+
+        // }
+
+        // };
+
+        RunSomething runSomething = () -> {
+
+        };
+    }
+}
---
+++ Java/Baek_Java8/java8to11/src/main/java/com/example/java8to11/RunSomething.java
@@ -0,0 +1,9 @@
+package com.example.java8to11;
+
+@FunctionalInterface
+public interface RunSomething {
+    // 추상메서드가 1개일 때 -> 함수형 인터페이스
+
+    void doIt(); // 추상메서드 abstaract 생략
+
+}
