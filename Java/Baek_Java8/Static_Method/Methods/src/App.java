716b3c2d52f4bc429af062471ed162f589a89e09
Jin <khjkhj1212@hanmail.net>
2021-07-05 23:14:42 +0900

    method

---
+++ Java/Baek_Java8/Static_Method/Methods/src/App.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        List<String> names = new ArrayList<>();
+        names.add("Jin");
+        names.add("Kim");
+        names.add("Toby");
+        names.add("Son");
+
+        names.forEach(System.out::println);
+
+        long k = names.stream().map(String::toUpperCase).filter(s -> s.startsWith("K")).count();
+
+        names.stream().map(String::toUpperCase).filter(s -> s.startsWith("K")).collect(Collectors.toSet());
+
+        System.out.println(k);
+
+        System.out.println("==========");
+
+        List<String> collect = names.stream().map((s) -> {
+            System.out.println(s);
+            return s.toUpperCase();
+        }).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+
+        System.out.println("==============");
+    }
+}
