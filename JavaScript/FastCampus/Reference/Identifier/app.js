6a6ea9c0f6214db3c4163b721e6478db94956e62
Jin <eful5678@gmail.com>
2021-09-07 23:15:11 +0900

    DB & JS

---
+++ JavaScript/FastCampus/Reference/Identifier/app.js
@@ -0,0 +1,19 @@
+
+// 식별자 -> age
+// 명령어, 키워드 -? let
+// 10 -? data
+let age = 10;
+
+// 식별자 -> setAge
+function setAge(){
+
+}
+
+// 속성 : 객체안에 있는 것
+// 식별자 -> age
+const o = {
+    age: 10,
+    ['myName']: '김' // 'myName' -> data
+}
+
+o.myName // 식별자
